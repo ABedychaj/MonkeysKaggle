87699485dc01d3f580d2888100c60ab3a18dac48
ABedychaj <andrzej.bedychaj@karhoo.com>
2020-05-09 12:44:31 +0200

    monkeysdataset added

--- src/dataset/MonkeyDataset.py
+++ src/dataset/MonkeyDataset.py
@@ -1,17 +1,71 @@
+import glob
+
 import kaggle
+from PIL import Image
 from torch.utils.data import Dataset
+
+label2name = {
+    'n0': 'alouatta_palliata',
+    'n1': 'erythrocebus_patas',
+    'n2': 'cacajao_calvus',
+    'n3': 'macaca_fuscata',
+    'n4': 'cebuella_pygmea',
+    'n5': 'cebus_capucinus',
+    'n6': 'mico_argentatus',
+    'n7': 'saimiri_sciureus',
+    'n8': 'aotus_nigriceps',
+    'n9': 'trachypithecus_johnii',
+}
+
+name2id = {
+    'alouatta_palliata': 0,
+    'erythrocebus_patas': 1,
+    'cacajao_calvus': 2,
+    'macaca_fuscata': 3,
+    'cebuella_pygmea': 4,
+    'cebus_capucinus': 5,
+    'mico_argentatus': 6,
+    'saimiri_sciureus': 7,
+    'aotus_nigriceps': 8,
+    'trachypithecus_johnii': 9,
+}
 
 
 class MonkeyDataset(Dataset):
 
-    def __init__(self, path, download=False):
+    def __init__(self, path, transform=None, phase='training', download=False):
         super(MonkeyDataset, self).__init__()
         if download:
             kaggle.api.authenticate()
             kaggle.api.dataset_download_files('slothkong/10-monkey-species', path=path, unzip=True)
+        else:
+            self.file_list = self.make_datapath_list(path, phase)
+            self.transform = transform
+            self.phase = phase
 
-    def __getitem__(self, index):
-        pass
+    @staticmethod
+    def make_datapath_list(root_path, phase):
+        path_list = []
+        for path in glob.glob(root_path + "/" + phase):
+            path_list.append(path)
+        return path_list
 
     def __len__(self):
-        pass
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+
+        # preprocessing
+        img_transformed = self.transform(img, self.phase)  # torch.Size([3,224,224])
+
+        # get image label from file name
+        arr = img_path.split('/')
+        label = arr[-2]
+        name = label2name[label]
+
+        # transform label to number
+        label_num = name2id[name]
+
+        return img_transformed, label_num
